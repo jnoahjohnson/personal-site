53fd9b42df4c3199e4911beb04b628e431f0712e
Noah Johnson <njohnson@elementum.com>
2024-04-26 17:48:18 -0600

    Fix site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
@@ -8,8 +8,8 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: "https://noahjohnson.dev",
   integrations: [mdx(), sitemap(), tailwind(), react()],
   output: "hybrid",
-  adapter: cloudflare()
+  adapter: cloudflare(),
 });
